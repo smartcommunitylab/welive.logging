f0223a1a728b1234aead05c292e717f89c8c1e9b
gioadami <giordano.adami@gmail.com>
2016-02-01 16:47:28 +0100

    Renamed 'aggregations' to 'aggs'.

--- src/main/java/it/smartcommunitylab/welive/logging/model/AggregationRequest.java
+++ src/main/java/it/smartcommunitylab/welive/logging/model/AggregationRequest.java
@@ -8,7 +8,7 @@ import java.util.Map;
 public class AggregationRequest {
 
 	private Map<String, Object> query;
-	private Map<String, Object> aggregations;
+	private Map<String, Object> aggs;
 	
 	public Map<String, Object> getQuery() {
 		return query;
@@ -16,11 +16,11 @@ public class AggregationRequest {
 	public void setQuery(Map<String, Object> query) {
 		this.query = query;
 	}
-	public Map<String, Object> getAggregations() {
-		return aggregations;
+	public Map<String, Object> getAggs() {
+		return aggs;
 	}
-	public void setAggregations(Map<String, Object> aggregations) {
-		this.aggregations = aggregations;
+	public void setAggs(Map<String, Object> aggregations) {
+		this.aggs = aggregations;
 	}
 	
 	
